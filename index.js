f95c7e96fa8e04e95ed49e5949935223cf947792
damonryon <damon.ryon@gmail.com>
2024-10-29 11:50:07 -0700

    refactor multiple cards with mock data

--- index.js
+++ index.js
@@ -3,72 +3,76 @@ http://api.weatherstack.com/current
     ? access_key = e0d9c4ac64bf2fecf325bc5eaa573b94
     & query = New York
 */
+// import 'dotenv/config';
+import  {mockData}  from "./mockData.js";
 
-// import  mockData  from "./mockData.js";
+// const mockData = {
+//     request: {
+//       type: 'City',
+//       query: 'Surprise, United States of America',
+//       language: 'en',
+//       unit: 'm'
+//     },
+//     location: {
+//       name: 'Surprise',
+//       country: 'United States of America',
+//       region: 'Arizona',
+//       lat: '33.631',
+//       lon: '-112.333',
+//       timezone_id: 'America/Phoenix',
+//       localtime: '2024-10-23 18:28',
+//       localtime_epoch: 1729708080,
+//       utc_offset: '-7.0'
+//     },
+//     current: {
+//       observation_time: '01:28 AM',
+//       temperature: 32,
+//       weather_code: 113,
+//       weather_icons: [
+//         'https://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0008_clear_sky_night.png'
+//       ],
+//       weather_descriptions: [ 'Clear' ],
+//       wind_speed: 4,
+//       wind_degree: 246,
+//       wind_dir: 'WSW',
+//       pressure: 1012,
+//       precip: 0,
+//       humidity: 12,
+//       cloudcover: 0,
+//       feelslike: 30,
+//       uv_index: 0,
+//       visibility: 16,
+//       is_day: 'no'
+//     }
+//   }
 
-const mockData = {
-    request: {
-      type: 'City',
-      query: 'Surprise, United States of America',
-      language: 'en',
-      unit: 'm'
-    },
-    location: {
-      name: 'Surprise',
-      country: 'United States of America',
-      region: 'Arizona',
-      lat: '33.631',
-      lon: '-112.333',
-      timezone_id: 'America/Phoenix',
-      localtime: '2024-10-23 18:28',
-      localtime_epoch: 1729708080,
-      utc_offset: '-7.0'
-    },
-    current: {
-      observation_time: '01:28 AM',
-      temperature: 32,
-      weather_code: 113,
-      weather_icons: [
-        'https://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0008_clear_sky_night.png'
-      ],
-      weather_descriptions: [ 'Clear' ],
-      wind_speed: 4,
-      wind_degree: 246,
-      wind_dir: 'WSW',
-      pressure: 1012,
-      precip: 0,
-      humidity: 12,
-      cloudcover: 0,
-      feelslike: 30,
-      uv_index: 0,
-      visibility: 16,
-      is_day: 'no'
-    }
-  }
-
-  const cities = ['Surprise', 'San Diego', 'Phoenix', 'Miami', 'Austin', 'Tacoma']
+  const cities = ['San Diego'] //, 'San Diego', 'Phoenix', 'Miami', 'Austin', 'Tacoma']
   let dataArr = []
 
 // console.log(mockData.location.name)
 
 // const cardCon = document.getElementsByClassName('card-container')
 
+// const weatherKey = '90fadf8e9ff2df5f1e9ea6c7761dfcda'
 
 // const weatherKey = process.env.WEATHER_API_KEY;
+// fetch(`http://api.weatherstack.com/current?access_key=${weatherKey}&query=${cities}`).then((el) => el.json()).then((el) => console.log(el))
 // console.log(weatherKey);
 // const body = document.body;
 
-// const weatherKey = '90fadf8e9ff2df5f1e9ea6c7761dfcda'
-
-cities.map(async (city) => {
-    let citySelector = await fetch(`http://api.weatherstack.com/current?access_key=${weatherKey}&query=${city}`)
-    let cityData = await citySelector.json()
-    dataArr.push(cityData)
-})
 
 
+// cities.forEach(async (city) => {
+//     let citySelector = await fetch(`http://api.weatherstack.com/current?access_key=${weatherKey}&query=${city}`)
+//     let cityData = await citySelector.json()
+//     console.log(cityData)
+//     console.log('dataArr', dataArr)
+//     return dataArr.push(cityData)
+// })
 
-console.log(dataArr)
+// console.log(dataArr)
+
+// console.log(mockData)
 
 // const weatherAPI = fetch(`http://api.weatherstack.com/current?access_key=${weatherKey}&query=Surprise`)
 
@@ -92,71 +96,78 @@ console.log(dataArr)
 
 
 const previewMaker = (data) => {
+    data.forEach((el, i) => {
+
     const card = document.createElement('div')
-    card.className = 'preview item'
+    card.className = 'preview item card-box'
     // card.id = 1
     document.getElementsByClassName('available-container')[0].append(card)
 
     const cardContents = document.createElement('div')
     cardContents.className = 'preview2'
-    document.getElementsByClassName('preview')[0].append(cardContents)
+    document.getElementsByClassName('card-box')[i].append(cardContents)
+    // console.log(document.getElementsByClassName('card-box'))
 
     const cardIcon = document.createElement('div')
     cardIcon.className = 'card-icon'
-    document.getElementsByClassName('preview2')[0].append(cardIcon)
+    document.getElementsByClassName('preview2')[i].append(cardIcon)
 
     const iconImg = document.createElement('img')
     iconImg.className = 'not-visible item'
-    iconImg.setAttribute('src', mockData.current.weather_icons[0])
-    document.getElementsByClassName('card-icon')[0].append(iconImg)
+    iconImg.setAttribute('src', el.current.weather_icons[0])
+    document.getElementsByClassName('card-icon')[i].append(iconImg)
 
     const cardData = document.createElement('div')
     cardData.className = 'card-data'
-    document.getElementsByClassName('preview2')[0].append(cardData)
+    document.getElementsByClassName('preview2')[i].append(cardData)
 
-    const cardCity = document.createElement('div')
-    cardCity.textContent = data.location.name
+    const cardCity = document.createElement('h1')
+    cardCity.textContent = el.location.name
     cardCity.className = 'card-city'
-    document.getElementsByClassName('card-data')[0].append(cardCity)
+    document.getElementsByClassName('card-data')[i].append(cardCity)
 
-    const cardState = document.createElement('div')
-    cardState.textContent = data.location.region
+    const cardState = document.createElement('h2')
+    cardState.textContent = el.location.region
     cardState.className = 'card-state'
-    document.getElementsByClassName('card-data')[0].append(cardState)
+    document.getElementsByClassName('card-data')[i].append(cardState)
 
-    const cardTemp = document.createElement('div')
-    cardTemp.textContent = `${data.current.temperature}°C`
+    const cardTemp = document.createElement('span')
+    cardTemp.textContent = `${el.current.temperature}°C`
     cardTemp.className = 'card-temp'
-    document.getElementsByClassName('card-data')[0].append(cardTemp)
+    document.getElementsByClassName('card-data')[i].append(cardTemp)
 
     const cardSub = document.createElement('div')
     cardSub.className = 'not-visible card-sub-data item'
-    document.getElementsByClassName('card-data')[0].append(cardSub)
+    document.getElementsByClassName('card-data')[i].append(cardSub)
 
-    const cardSky = document.createElement('div')
-    cardSky.textContent = `Sky: ${data.current.weather_descriptions[0]}`
+    const cardSky = document.createElement('span')
+    cardSky.textContent = `Sky: ${el.current.weather_descriptions[0]}`
     cardSky.className = 'card-sky'
-    document.getElementsByClassName('card-sub-data')[0].append(cardSky)
+    document.getElementsByClassName('card-sub-data')[i].append(cardSky)
 
-    const cardTime = document.createElement('div')
-    cardTime.textContent = `Time: ${data.location.localtime}`
+    const cardTime = document.createElement('span')
+    cardTime.textContent = `Time: ${el.location.localtime}`
     cardTime.className = 'card-time'
-    document.getElementsByClassName('card-sub-data')[0].append(cardTime)
+    document.getElementsByClassName('card-sub-data')[i].append(cardTime)
 
-    const cardWind = document.createElement('div')
-    cardWind.textContent = `Wind Speed: ${data.current.wind_speed} MPH`
+    const cardWind = document.createElement('span')
+    cardWind.textContent = `Wind Speed: ${el.current.wind_speed} MPH`
     cardWind.className = 'card-wind-speed'
-    document.getElementsByClassName('card-sub-data')[0].append(cardWind)
+    document.getElementsByClassName('card-sub-data')[i].append(cardWind)
 
-    const cardWindDir = document.createElement('div')
-    cardWindDir.textContent = `Wind Dir: ${data.current.wind_dir}`
+    const cardWindDir = document.createElement('span')
+    cardWindDir.textContent = `Wind Dir: ${el.current.wind_dir}`
     cardWindDir.className = 'card-wind-dir'
-    document.getElementsByClassName('card-sub-data')[0].append(cardWindDir)
+    document.getElementsByClassName('card-sub-data')[i].append(cardWindDir)
 
-    const cardHumidity = document.createElement('div')
-    cardHumidity.textContent = `Humidity: ${data.current.humidity}%`
+    const cardHumidity = document.createElement('span')
+    cardHumidity.textContent = `Humidity: ${el.current.humidity}%`
     cardHumidity.className = 'card-humidity'
-    document.getElementsByClassName('card-sub-data')[0].append(cardHumidity)
+    document.getElementsByClassName('card-sub-data')[i].append(cardHumidity)
+
+    })
+
+
 }
 
 
@@ -228,14 +239,22 @@ const cardMaker = (data) => {
 
 // cardMaker(mockData)
 
-// previewMaker(mockData)
+previewMaker(mockData)
+
+// previewMaker(cities)
+
+// previewMaker(dataArr)
+
+// console.log('mockData', mockData)
+
+
 
 for (let el of dataArr){
     previewMaker(el)
 }
 
 
-const allItems = document.querySelectorAll('.item')
+const allItems = document.querySelectorAll('.card-box')
 const main = document.getElementById('main')
 const favs = document.getElementById('faves')
 
--- mockData.js
+++ mockData.js
@@ -1,4 +1,5 @@
-const mockData = {
+export const mockData = [
+  {
   request: {
     type: 'City',
     query: 'Surprise, United States of America',
@@ -36,4 +37,84 @@ const mockData = {
     visibility: 16,
     is_day: 'no'
   }
+},
+{
+  request: {
+    type: 'City',
+    query: 'San Diego, United States of America',
+    language: 'en',
+    unit: 'm'
+  },
+  location: {
+    name: 'San Diego',
+    country: 'United States of America',
+    region: 'California',
+    lat: '32.715',
+    lon: '-117.156',
+    timezone_id: 'America/Los_Angeles',
+    localtime: '2024-10-28 18:21',
+    localtime_epoch: 1730139660,
+    utc_offset: '-7.0'
+  },
+  current: {
+    observation_time: '01:21 AM',
+    temperature: 18,
+    weather_code: 116,
+    weather_icons: [
+      'https://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0004_black_low_cloud.png'
+    ],
+    weather_descriptions: [ 'Partly cloudy' ],
+    wind_speed: 25,
+    wind_degree: 274,
+    wind_dir: 'W',
+    pressure: 1013,
+    precip: 0,
+    humidity: 73,
+    cloudcover: 50,
+    feelslike: 18,
+    uv_index: 0,
+    visibility: 16,
+    is_day: 'no'
+  }
+},
+{
+  "request": {
+      "type": "City",
+      "query": "New York, United States of America",
+      "language": "en",
+      "unit": "m"
+  },
+  "location": {
+      "name": "New York",
+      "country": "United States of America",
+      "region": "New York",
+      "lat": "40.714",
+      "lon": "-74.006",
+      "timezone_id": "America/New_York",
+      "localtime": "2019-09-07 08:14",
+      "localtime_epoch": 1567844040,
+      "utc_offset": "-4.0"
+  },
+  "current": {
+      "observation_time": "12:14 PM",
+      "temperature": 13,
+      "weather_code": 113,
+      "weather_icons": [
+          "https://assets.weatherstack.com/images/wsymbols01_png_64/wsymbol_0001_sunny.png"
+      ],
+      "weather_descriptions": [
+          "Sunny"
+      ],
+      "wind_speed": 0,
+      "wind_degree": 349,
+      "wind_dir": "N",
+      "pressure": 1010,
+      "precip": 0,
+      "humidity": 90,
+      "cloudcover": 0,
+      "feelslike": 13,
+      "uv_index": 4,
+      "visibility": 16
+  }
 }
+]
